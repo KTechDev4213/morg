949b675c2ffc5a46e9ab158a36a24c4d9f70cc29
Yitzc <Yitzc@DESKTOP-4K6U634>
2024-03-28 19:56:57 -0400

    Artist and Album sorting functionality fully functional.

--- morg/Program.cs
+++ morg/Program.cs
@@ -2,37 +2,33 @@
 
 string dir = Directory.GetCurrentDirectory();
 
-string helpString = "";
+string helpString = "Program Help" +
+    "\nCOMMANDS\n artist: Sort files into folders by artist";
 string badParams = "shoots";
-if (args[0] == "help")
-{
-    Console.Write(helpString);
 
-}
-else if (args[0].StartsWith("-"))
+foreach (string arg in args)
 {
-    args[0] = args[0].Remove(0);
-    foreach (char c in args[0])
+    if (arg == "help")
     {
-        switch (c)
-        {
-            case 's':
-                Console.WriteLine("Sorting Artists...");
-                SortArtists(dir);
-                break;
-            case 'a':
-                SortAlbums(dir); break;
-            case 'g':
-                SortGenres(dir); break;
-            default:
-                Console.Write(badParams);
-                break;
-                //return;
-        }
-        
+        Console.Write(helpString);
+
     }
-    SortArtists(dir);
+    else if (arg == "artist")
+    {
+        {
+            SortArtists(dir);
+        }
+    }
+    else if (arg == "album")
+    {
+        SortAlbums(dir);
+    }
+    else
+    {
+        Console.WriteLine(badParams);
+    }
 }
+SortAlbums(dir);
 void SortArtists(string folder)
 {
     foreach(string file in  Directory.GetFiles(folder))
@@ -41,25 +37,36 @@ void SortArtists(string folder)
         string artist = tag.Tag.FirstArtist;
         if (artist != null && artist != "")
         {
-            if (Directory.Exists(artist))
-            {
-                Console.WriteLine(Path.Join(artist, file));
-                System.IO.File.Move(file, Path.Join(artist, file));
-            }
-            else
+            string newPath = Path.Join(Path.GetDirectoryName(file), artist, Path.GetFileName(file));
+            if (!Directory.Exists(artist))
             {
                 Directory.CreateDirectory(artist);
-                Console.WriteLine(Path.Join(artist, file));
-                Console.WriteLine(file);
-                System.IO.File.Move(file, Path.Join(artist, file));
             }
+            Console.WriteLine(newPath);
+            System.IO.File.Move(file, newPath);
+
         }
 
     }
 }
 void SortAlbums(string folder)
 {
+    foreach(string file in Directory.GetFiles(folder))
+    {
+        TagLib.File tag = TagLib.File.Create(file);
+        string album = tag.Tag.Album;
+        if(album != null && album != "")
+        {
+            string newPath = Path.Join(Path.GetDirectoryName(file), album, Path.GetFileName(file));
+            if(!Directory.Exists(album))
+            {
+                Directory.CreateDirectory(album);
+            }
+            Console.WriteLine(newPath);
+            System.IO.File.Move(file, newPath);
 
+        }
+    }
 }
 void SortGenres(string folder)
 {
